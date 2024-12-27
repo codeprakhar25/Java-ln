967c5668b645cc00ca903a409a97c5d0f7e451ce
unknown <prakharkhatri123@gmail.com>
2024-12-27 18:55:00 +0530

    feat: kafka init

---
+++ src/main/java/com/example/first/demo/kafka/KafkaSender.java
@@ -0,0 +1,20 @@
+package com.example.first.demo.kafka;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+// import lombok.extern.slf4j.Slf4j;
+
+@Component
+// @Slf4j
+public class KafkaSender {
+// @Autowired
+//     private KafkaTemplateString, String> kafkaTemplate;
+
+//         public void sendMessage(String message, String topicName) {
+//         log.info("Sending : {}", message);
+//         log.info("--------------------------------");
+
+//         kafkaTemplate.send(topicName, message);
+//     }  
+}
